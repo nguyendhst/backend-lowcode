e48c42ed274927b30021fa71080a38374525227f
NgTrNghia1211 <luckilyislife@gmail.com>
2023-09-28 11:22:38 +0700

    example module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { database_config } from '@configs/configuration.config';
-
+import { ExampleModule } from '@modules/example/example.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ 
       isGlobal: true,
       envFilePath: process.env.NODE_ENV === 'development' ? '.env.dev' : '.env',
       load: [database_config], 
-    })
+    }),
+    ExampleModule,
   ],
   controllers: [AppController],
   providers: [AppService],
