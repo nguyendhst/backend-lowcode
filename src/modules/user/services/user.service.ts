6d798e21886edaf51f2cd3e769f8042b8dd99eae
NgTrNghia1211 <luckilyislife@gmail.com>
2023-10-19 18:44:18 +0700

    change connection to mysql

--- src/modules/user/services/user.service.ts
+++ src/modules/user/services/user.service.ts
@@ -5,6 +5,8 @@ import { UserDTO } from '@dtos/user.dto';
 
 @Injectable()
 export class UserService {
+  constructor(private prisma: PrismaService) {}
+
   async createUser(data: Prisma.UserCreateInput): Promise<User> {
     return this.prisma.user.create({
       data,
@@ -14,7 +16,6 @@ export class UserService {
   async findByUsername(username: string): Promise<User> {
     throw new Error('Method not implemented.');
   }
-  constructor(private prisma: PrismaService) {}
 
   async getUserById(id: number): Promise<User> {
     return this.prisma.user.findUnique({ where: { id } });
