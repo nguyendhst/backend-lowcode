65045c3c1df428fc9fa134544e26e60a225b7534
nguyendhst <nguyendhst@proton.me>
2023-11-03 16:13:58 +0700

    moved back to url scheme

--- src/modules/auth/controllers/oauth.controller.ts
+++ src/modules/auth/controllers/oauth.controller.ts
@@ -10,11 +10,12 @@ import { AuthGuard } from '@nestjs/passport';
 import { AuthenticationService } from '@services/authentication.service';
 import { Public } from '@decorators/public-route.decorator';
 import { RedirectingExceptionFilter } from '@filters/redirect-filter';
-import { TokenSuccessResponse } from '@dtos/auth.dto';
+import { ApiConfigService } from '@shared/services/api-config.service';
 @Controller('oauth')
 export class OAuthController {
   constructor(
     private readonly authService: AuthenticationService,
+    private readonly apiConfigService: ApiConfigService,
   ) {}
 
   @Get('google')
@@ -26,13 +27,14 @@ export class OAuthController {
   @Get('google/callback')
   @Public()
   @UseGuards(AuthGuard('google'))
-  async googleLoginCallback(@Req() req): Promise<TokenSuccessResponse> {
+  async googleLoginCallback(@Req() req, @Res() res) {
     const user = req.user;
     const tokens: [string, string] = await this.authService.login(user);
 
-    return {
-      AccessToken: tokens[0],
-      RefreshToken: tokens[1],
-    };
+    const redirectUrl = `${this.apiConfigService.app.clientUrl}/api/auth`;
+    // Add tokens to the redirect url
+    const redirectUrlWithToken = `${redirectUrl}&accessToken=${tokens[0]}&refreshToken=${tokens[1]}`;
+
+    res.redirect(redirectUrlWithToken);
   }
 }
